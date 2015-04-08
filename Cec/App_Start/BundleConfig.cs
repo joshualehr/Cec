45a97188706ce31f1ba2dc521a8cbda5e7ec95f7
joshualehr <joshuadlehr@hotmail.com>
2015-04-07 21:39:43 -0400

    Changed function to select all chechboxes

--- Cec/App_Start/BundleConfig.cs
+++ Cec/App_Start/BundleConfig.cs
@@ -16,19 +16,18 @@ namespace Cec
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr")
-                .Include("~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/admin/scripts")
                 .Include("~/Scripts/jquery-2.1.3.js")
                 .Include("~/Scripts/jquery.validate.js")
                 .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/modernizr-2.8.3")
                 .Include("~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/scripts")
                 .Include("~/Scripts/jquery-2.1.3.js")
                 .Include("~/Scripts/jquery.validate.js")
                 .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/modernizr-2.8.3")
                 .Include("~/Scripts/bootstrap.js"));
         }
     }
