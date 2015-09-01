01d881213128b75e03b1bfc03cfda6e2dee3c7a0
joshualehr <joshuadlehr@hotmail.com>
2015-08-31 20:28:04 -0400

    Update after installing VS 2015

--- Cec/Areas/Admin/Controllers/UserController.cs
+++ Cec/Areas/Admin/Controllers/UserController.cs
@@ -1,6 +1,9 @@
-﻿using Cec.Helpers;
+﻿using Cec.Areas.Admin.ViewModels;
+using Cec.Helpers;
+using Cec.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +17,123 @@ namespace Cec.Areas.Admin.Controllers
         // GET: Admin/User
         public ActionResult Index()
         {
-            return View();
+            return View(new UserIndexViewModel());
+        }
+
+        //Get: /Admin/Create
+        public ActionResult Create()
+        {
+            return View(new UserCreateViewModel());
+        }
+
+        //POST: /Admin/Create
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(UserCreateViewModel form)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", new { id = form.Create() });
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(form);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var user = db.AspNetUsers.Find(id);
+            if (user == null)
+                return HttpNotFound();
+
+            return View(new UserEdit
+            {
+                UserName = user.UserName,
+                Roles = db.AspNetRoles.Select(role => new RoleCheckBox
+                {
+                    Id = role.Id,
+                    IsChecked = user.AspNetRoles.Contains(role),
+                    Name = role.Name
+                }).ToList()
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, UserEdit form)
+        {
+            var user = new User(id);
+            if (user == null)
+                return HttpNotFound();
+
+            SyncRoles(form.Roles, user.Roles);
+
+            if (db.AspNetUsers.Any(u => u.UserName == form.UserName && u.Id != id))
+                ModelState.AddModelError("UserName", "UserName must be unique.");
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            user.UserName = form.UserName;
+
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public ActionResult ResetPassword(string id)
+        {
+            var user = new User(id);
+            if (user == null)
+                return HttpNotFound();
+
+            return View(new UserResetPasswordViewModel
+            {
+                UserName = user.UserName
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult ResetPassword(string id, UserResetPasswordViewModel form)
+        {
+            var user = new User(id);
+            if (user == null)
+                return HttpNotFound();
+            form.UserName = user.UserName;
+            if (!ModelState.IsValid)
+                return View(form);
+            user.SetPassword(form.Password);
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            
+            return RedirectToAction("index");
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Delete(string id)
+        {
+            var user = new AspNetUser(id);
+            if (user == null)
+                return HttpNotFound();
+            db.AspNetUsers.Remove(user);
+            db.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                var db = new ApplicationDbContext();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Cec/Areas/Admin/ViewModels/User.cs
@@ -0,0 +1,149 @@
+﻿using Cec.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Cec.Areas.Admin.ViewModels
+{
+    public class RoleCheckBox
+    {
+        public string Id { get; set; }
+        public bool IsChecked { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class User
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private const int WorkFactor = 13;
+
+        public static void FakeHash()
+        {
+            BCrypt.Net.BCrypt.HashPassword("", WorkFactor);
+        }
+
+        public virtual string Id { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string PasswordHash { get; set; }
+
+        public virtual ICollection<AspNetRole> Roles { get; set; }
+
+        public User()
+        {
+            Roles = new List<AspNetRole>();
+        }
+
+        public User(string id)
+        {
+            var user = db.AspNetUsers.Find(id);
+            this.Id = user.Id;
+            this.UserName = user.UserName;
+            this.PasswordHash = user.PasswordHash;
+            this.Roles = user.AspNetRoles;
+        }
+
+        public virtual void SetPassword(string password)
+        {
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public virtual bool CheckPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
+        }
+    }
+
+    //GET: /Admin/User
+    public class UserIndexViewModel
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<AspNetUser> Users { get; set; }
+
+        public UserIndexViewModel()
+        {
+            this.Users = db.AspNetUsers;
+        }
+    }
+
+    //GET: /Admin/User/Create
+    public class UserCreateViewModel
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IList<RoleCheckBox> Roles { get; set; }
+
+        [Required, MaxLength(128)]
+        public string UserName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public UserCreateViewModel()
+        {
+            var Roles = db.AspNetRoles.Select(role => new RoleCheckBox
+                {
+                    Id = role.Id,
+                    IsChecked = false,
+                    Name = role.Name
+                }).ToList();
+        }
+
+        public string Create()
+        {
+            var user = new User();
+            SyncRoles(this.Roles, user.Roles);
+
+            if (db.AspNetUsers.Any(u => u.UserName == this.UserName))
+                ModelState.AddModelError("UserName", "UserName must be unique.");
+
+            user.UserName = this.UserName;
+            user.SetPassword(this.Password);
+
+            db.SaveChanges();
+
+            return this.UserName;
+        }
+
+        private void SyncRoles(IList<RoleCheckBox> checkboxes, ICollection<AspNetRole> roles)
+        {
+            var selectedRoles = new List<AspNetRole>();
+
+            foreach (var role in db.AspNetRoles)
+            {
+                var checkbox = checkboxes.Single(c => c.Id == role.Id);
+                checkbox.Name = role.Name;
+
+                if (checkbox.IsChecked)
+                    selectedRoles.Add(role);
+            }
+
+            foreach (var toAdd in selectedRoles.Where(t => !roles.Contains(t)))
+                roles.Add(toAdd);
+
+            foreach (var toRemove in roles.Where(t => !selectedRoles.Contains(t)).ToList())
+                roles.Remove(toRemove);
+        }
+    }
+
+    //GET: /Admin/User/Edit/5
+    public class UserEditViewModel
+    {
+        public IList<RoleCheckBox> Roles { get; set; }
+
+        [Required, MaxLength(128)]
+        public string UserName { get; set; }
+    }
+
+    public class UserResetPasswordViewModel
+    {
+        public string UserName { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
