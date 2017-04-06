e981fed460e06e5b0d62428746816df78699b243
joshualehr <joshuadlehr@hotmail.com>
2017-04-06 18:32:03 -0400

    Updated PhaseQuantities migration

---
+++ Cec/Migrations/201704062227546_PhaseQuantities1.cs
@@ -0,0 +1,24 @@
+namespace Cec.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PhaseQuantities1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AreaMaterial", "RoughQuantity", c => c.Double(nullable: false));
+            AddColumn("dbo.AreaMaterial", "FinishQuantity", c => c.Double(nullable: false));
+            AddColumn("dbo.ModelMaterial", "RoughQuantity", c => c.Double(nullable: false));
+            AddColumn("dbo.ModelMaterial", "FinishQuantity", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ModelMaterial", "FinishQuantity");
+            DropColumn("dbo.ModelMaterial", "RoughQuantity");
+            DropColumn("dbo.AreaMaterial", "FinishQuantity");
+            DropColumn("dbo.AreaMaterial", "RoughQuantity");
+        }
+    }
+}
